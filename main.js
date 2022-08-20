d2337e5803c5aea1aed331f97228218822f4e712
Ethan Sawicki <ethansawicki2797@gmail.com>
2022-08-20 09:48:13 -0500

    adding the js file

---
+++ main.js
@@ -0,0 +1,41 @@
+const buyClay = () => {
+   const clay = {}
+   return clay
+}
+
+const makePottery = (pottery) => {
+    pottery.shape = "Bowl"
+    return pottery
+}
+
+const bisqueFire = (fire) => {
+    fire.readyForGlazing = true
+    return fire
+}
+
+const glazePottery = (glaze) => {
+    if(glaze.readyForGlazing === true) {
+        glaze.glazing = "Midnight Blue"
+        return glaze
+    }
+}
+
+const finalFiring = (pottery, temp) => {
+    if(temp > 1200) {
+        pottery.temp = temp
+        pottery.cracked = true
+        return pottery
+    } else {
+        pottery.temp = temp
+        pottery.cracked = false
+        return pottery
+    }
+}
+
+const clay = buyClay()
+const pottery = makePottery(clay)
+const bisque = bisqueFire(pottery)
+const glaze = glazePottery(bisque)
+const finalProduct = finalFiring(glaze, 1200)
+
+console.log(finalProduct)
